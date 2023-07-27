13a7b2776a0c8906d99e81fe77050e82961ec282
Alf-Muir <t-alfredmuir@microsoft.com>
2023-07-27 11:51:38 +0100

    new

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-from flask import Flask, request, redirect, url_for, render_template
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+# app.py
+from flask import Flask
 
 app = Flask(__name__)
 
-connect_str = "DefaultEndpointsProtocol=https;AccountName=alfstorageaccount1;AccountKey=1wWmxeeecJgf0gURR6hYWGKPHKB1gc6w00mWq6DBVlU8I3o6vwypv18Dwu6muCXxFNb+3hskz5ol+AStF9JAsQ==;EndpointSuffix=core.windows.net"
-container_name = "container1"
-
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/', methods=['POST'])
-def upload_file():
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-        blob_client = blob_service_client.get_blob_client(container_name, uploaded_file.filename)
-        blob_client.upload_blob(uploaded_file.read())
-    return redirect(url_for('index'))
+def home():
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
